585b68d28c582a9f9dc89015b5e602e9ca3e96d0
Jeremy Bruestle <jeremy.bruestle@gmail.com>
2015-01-12 15:13:26 -0800

    Make things actually easy to install

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='flock',
+    description='Peer to peer app synchronization protocol',
+    url='https://github.com/jbruestle/flock',
+    license='MIT',
+    author='Jeremy Bruestle',
+    author_email='jeremy.bruestle@gmail.com',
+    version='0.1',
+    packages=['flock'],
+    scripts=['scripts/flock'],
+    install_requires=[
+        'bintrees', 
+        'pycrypto',
+        'bencode',
+        'miniupnpc',
+        'netifaces',
+        'simplejson',
+        'ipaddr',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Networking',
+    ],
+)
